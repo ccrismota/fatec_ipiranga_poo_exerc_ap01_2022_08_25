eb55c1341aeccfa18be65bbee3faf5f43b146a96
ccrismota <ccrismota@gmail.com>
2022-09-25 16:39:06 -0300

    Realização dos ajustes necessários para funcionamento da classe TesteVeiculo

--- TesteVeiculo.java
+++ TesteVeiculo.java
@@ -1,38 +1,15 @@
 import javax.swing.JOptionPane;
 public class TesteVeiculo {
     public static void main(String[] args) {
-        Veiculo carro1 = new Veiculo();
+        Veiculo carro1 = new Veiculo("GOL", 2018, 140.0);
 
-        String modelo1 = JOptionPane.showInputDialog("Digite modelo para o carro I");
-        String ano1 = JOptionPane.showInputDialog("Digite o ano para o carro I");
-        int anoModeloUm = Integer.parseInt(ano1);
-        String velocidadeModeloUm = JOptionPane.showInputDialog("Digite a velocidade");
-        double velocidadeUm = Double.parseDouble(velocidadeModeloUm);
-
-        carro1.setModelo(modelo1);
-        carro1.setAno(anoModeloUm);
-        carro1.setVelocidade(velocidadeUm);
-
-        System.out.println();
-        System.out.println("Carro I: " + carro1.getModelo() + " Ano: " + carro1.getAno() +" Velocidade Atual: " + carro1.getVelocidade()+"\n");
+        System.out.println("\n"+"Carro I: " + carro1.getModelo() + " - Ano: " + carro1.getAno() +" - Velocidade Atual: " + carro1.getVelocidade()+"\n");
         carro1.acelerar(carro1.getModelo());
         carro1.parar(carro1.getModelo());
 
-        Veiculo carro2 = new Veiculo();
+        Veiculo carro2 = new Veiculo("HB20X", 2020, 110.0);
 
-        String modelo2 = JOptionPane.showInputDialog("Digite modelo para o carro II");
-        String ano2 = JOptionPane.showInputDialog("Digite o ano para o carro II");
-        int anoModeloDois = Integer.parseInt(ano2);
-        String velocidadeModeloDois = JOptionPane.showInputDialog("Digite a velocidade");
-        double velocidadeDois = Double.parseDouble(velocidadeModeloDois);
-
-
-        carro2.setModelo(modelo2);
-        carro2.setAno(anoModeloDois);
-        carro2.setVelocidade(velocidadeDois);
-
-        System.out.println();
-        System.out.println("Carro II: " + carro2.getModelo() + " Ano: " + carro2.getAno() +" Velocidade Atual: " + carro2.getVelocidade()+"\n");
+        System.out.println("\n"+"Carro II: " + carro2.getModelo() + " Ano: " + carro2.getAno() +" Velocidade Atual: " + carro2.getVelocidade()+"\n");
         carro2.acelerar(carro2.getModelo());
         carro2.parar(carro2.getModelo());
     }
